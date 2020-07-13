b51863dd14cdb0a2693fd5f2d113734a0e1b62e0
abdoulzak <38372413+abdoulzak@users.noreply.github.com>
2020-07-13 17:43:24 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AppearanceProvider } from 'react-native-appearance';
+
+import { Main } from './src/main';
+
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <AppearanceProvider>
+        <Main />
+      </AppearanceProvider>
+    </SafeAreaProvider>
+  );
+}
